b1cf8de938123bf7af0fe57af83acda842c68546
VnySrc <vinyprog.work@gmail.com>
2022-03-27 18:32:19 -0300

    y

--- src/controllers/uploadsCrontroller.ts
+++ src/controllers/uploadsCrontroller.ts
@@ -1,8 +1,14 @@
 import {Request, Response} from "express"
 
 export const uploadFile = (req: Request, res: Response) => {
+    if (req.file) {
     const perfil = req.file;
-
     console.log(perfil)
+
     res.json({})
+    }
+    else{
+    res.status(400)
+    res.json({})
+    }
 }
